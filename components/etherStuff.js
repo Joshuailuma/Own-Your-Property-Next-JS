cc9a783ad796c1c47636f2443f241fa94d976f56
Joshua Iluma <joshuailuma@gmail.com>
2023-01-19 18:05:51 +0100

    Update etherStuff.js

--- components/etherStuff.js
+++ components/etherStuff.js
@@ -1,6 +1,6 @@
 import ethers from 'ethers'
 
-const provider = ethers.providers.JsonRpcProvider('https://eth-goerli.alchemyapi.io/v2/6mNAPvD9Gse0BojzyDSBrNb2S_1dOTCa')
+const provider = ethers.providers.JsonRpcProvider('https://eth-goerli.alchemyapi.io/v2/')
 const signer = provider.getSigner()
 console.log(signer);
 
